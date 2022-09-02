325e6e6d2fb9d488d8490d74da1949cf4a5886ac
ohhi <onni.ylinen@googlemail.com>
2022-09-02 13:36:10 +0300

    Update build.py

--- build.py
+++ build.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         system('conan test_package -s compiler="Visual Studio" -s compiler.runtime=MDd -s build_type=Debug -o OpenCV:shared=False %s' % params)
         system('conan test_package -s compiler="Visual Studio" -s compiler.runtime=MDd -s build_type=Debug -o OpenCV:shared=True %s' % params)
     else:
-        system('conan test_package -s build_type=Release -o OpenCV:shared=False %s' % params)
-        system('conan test_package -s build_type=Debug -o OpenCV:shared=False %s' % params)
-        system('conan test_package -s build_type=Release -o OpenCV:shared=True %s' % params)
-        system('conan test_package -s build_type=Debug -o OpenCV:shared=True %s' % params)
+        system('conan create . jilab/stable -s build_type=Release -o OpenCV:shared=False %s' % params)
+        system('conan create . jilab/stable -s build_type=Debug -o OpenCV:shared=False %s' % params)
+        system('conan create . jilab/stable -s build_type=Release -o OpenCV:shared=True %s' % params)
+        system('conan create . jilab/stable -s build_type=Debug -o OpenCV:shared=True %s' % params)
